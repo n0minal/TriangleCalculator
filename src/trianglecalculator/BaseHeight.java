3fa8315309789bb3cb31f519e412d7ed3d70b767
LEONARDO MOURA <2016103732@UC.ACAD>
2017-08-31 10:17:59 -0300

    First commit

---
+++ src/trianglecalculator/BaseHeight.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trianglecalculator;
+
+/**
+ *
+ * @author 2016103732
+ */
+public class BaseHeight {
+    private double base;
+    private double height;
+    public double Area;
+    
+    public double getArea() {
+        return Area;
+    }
+    
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    
+    
+}
